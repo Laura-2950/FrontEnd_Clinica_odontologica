0a7f4a8748b670ec21ed0b35f0e66c37f01eafdf
Laura-2950 <mlaurafernandez84@gmail.com>
2022-04-15 17:07:34 -0300

    Tercer Commit

--- src/componente/home.js
+++ src/componente/home.js
@@ -10,7 +10,7 @@ export default function Home() {
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <img src="img-1.jpg" className="d-block w-100" alt="..." />
+                    <img src="img-0.jpg" className="d-block w-100" alt="..." />
                 </div>
                 <div className="carousel-item">
                     <img src="img-2.jpg" className="d-block w-100" alt="..." />
